ed6c9bf9167f85650dc2c82bec90f41bf5e1bc66
chakerasman <chaker.asm@gmail.com>
2023-01-19 17:58:26 +0100

    Display Products & Pagination & SearchByDesignation

--- src/app/pages/products/products-routing.module.ts
+++ src/app/pages/products/products-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddproductComponent } from './addproduct/addproduct.component';
 import { ListproductsComponent } from './listproducts/listproducts.component';
 import { ProductsComponent } from './products.component';
 
@@ -10,7 +11,11 @@ const routes: Routes = [{
     {
       path: 'list',
       component: ListproductsComponent,
-    }
+    }  ,
+    {
+      path: 'add',
+      component: AddproductComponent,
+    },
   ]
   }]
 
