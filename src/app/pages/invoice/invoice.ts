8db7bfc6c9cb5799cbcbc93d8ebd4f64aa379695
Abdelkhalak rbati <rbati.dev@gmail.com>
2023-01-20 00:27:09 +0100

    create models invoice and customer

--- src/app/pages/invoice/invoice.ts
+++ src/app/pages/invoice/invoice.ts
@@ -1,2 +1,12 @@
+import { Customer } from "../customers/customers"
+import { Product } from "../products/products"
+
 export class Invoice {
+
+    "id": string;
+    "customer": Customer;
+    "products": Product[];
+    "invoiceDate":Date;
+    "paymentType":string;
+    "discount": number;
 }
